dfbfb10c9c39159cbc55c87b5f08281bad4b7cd3
Congying Zhao <t3cozh00@students.oamk.fi>
2025-02-13 00:05:26 +0200

    screenshot for visuals

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,7 @@
 import { ScrollView } from "react-native";
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import LoginView from "./LoginView";
-import SignupView from "./SignupView";
 
 const index = () => {
   return (
@@ -14,10 +12,3 @@ const index = () => {
 };
 
 export default index;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//   },
-// });
